37bcac4f2eee3e115d8984c3d6633b79d6ce3d63
Muhammad Fauzan <muhammadfauzan3f@gmail.com>
2025-01-13 19:44:18 +0700

    Fix Models

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,7 +17,8 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->string('role');
+            $table->enum('role', ['admin', 'user']);
+            $table->enum('user_role', ['free', 'premium']);
             $table->rememberToken();
             $table->timestamps();
         });
